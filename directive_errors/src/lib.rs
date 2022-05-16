d5ba470e9bd73d322b9240decbf06b479fa0bdc4
grostaco <grostaco@gmail.com>
2022-05-16 12:00:54 +0700

    feat: add missing positional argument error

--- directive_errors/src/lib.rs
+++ directive_errors/src/lib.rs
@@ -4,4 +4,7 @@ use thiserror::Error;
 pub enum DirectiveParseError {
     #[error("cannot parse {0} as {1}: {2}")]
     CannotParse(String, String, String),
+
+    #[error("expected argument {0} at position {1}")]
+    ExpectPositionalArgument(String, usize),
 }

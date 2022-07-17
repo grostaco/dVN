f81e256eb69ecd36a4e19dbcf7a3b1730e98d0f6
grostaco <grostaco@gmail.com>
2022-07-17 23:20:55 +0700

    refactor: commented out the file

--- src/parser/config.rs
+++ src/parser/config.rs
@@ -1,14 +1,14 @@
-use std::{collections::HashMap, fs::File, path::Path, io};
+// use std::{collections::HashMap, fs::File, path::Path, io};
 
-pub struct Config(HashMap<String, HashMap<String, String>>);
+// pub struct Config(HashMap<String, HashMap<String, String>>);
 
-impl Config {
-    pub fn open<P: AsRef<Path>>(path: P) -> io::Result<Self> {
-        let mut fields = HashMap::new();
-        let mut file = File::open(path)?;
+// impl Config {
+//     pub fn open<P: AsRef<Path>>(path: P) -> io::Result<Self> {
+//         let mut fields = HashMap::new();
+//         let mut file = File::open(path)?;
 
         
 
-        Ok(Self (fields))
-    }
-}
+//         Ok(Self (fields))
+//     }
+// }

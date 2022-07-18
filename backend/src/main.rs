af1c295c30fb1ae2ef47e52777b23bb577d52a11
grostaco <grostaco@gmail.com>
2022-07-18 11:07:14 +0700

    feat: endpoint for engine access

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,14 +1,14 @@
-use std::{path::Path, fs::{self, OpenOptions}, io::Write, sync::Mutex};
+use std::{path::Path, fs::{self, OpenOptions}, io::{Write, self}, sync::Mutex};
 use image_rpg::core::engine::Engine;
-use rocket::{State, serde::json::Json, http::Status, response::status};
+use rocket::tokio::fs::{remove_dir, create_dir};
 use serde::Serialize;
 use serde_json::json;
 
 #[macro_use] extern crate rocket;
 
-struct EngineWrapper {
-    engine: Mutex<Option<Engine>>,
-}
+// struct EngineWrapper {
+//     engine: Mutex<Option<Engine>>,
+// }
 
 #[derive(Serialize)]
 #[serde(crate = "rocket::serde")]
@@ -18,24 +18,45 @@ struct RenderResult {
 
 #[get("/api/rendered/<id>/preview.png")]
 fn image_preview(id: u64) -> std::io::Result<Vec<u8>> {
-    let p = Path::new("../assets/rendered/.cache").join(id.to_string()).with_extension("png");
+    let p = Path::new("../assets/rendered/").join(id.to_string()).with_extension("png");
     fs::read(p)
 }
 
-#[get("/api/render", data = "<content>")]
-fn render(engine_wrapper: &State<EngineWrapper>, content: &str) -> String {
+#[post("/api/render", data = "<content>")]
+fn render(content: &str) -> String {
     let p = Path::new("../assets/autogen_script.script");
-    let mut engine = engine_wrapper.engine.lock().unwrap();
+    println!("Content:\n{}", content);
 
-    OpenOptions::new().write(true).truncate(true).open(p).unwrap().write_all(content.as_bytes()).unwrap();
-    engine.replace(Engine::new(p.to_str().unwrap()).unwrap());
+    OpenOptions::new().create(true).write(true).truncate(true).open(p).unwrap().write_all(content.as_bytes()).unwrap();
+    let mut engine = match Engine::new(p.to_str().unwrap()) {
+        Ok(engine) => engine,
+        Err(e) => return json!({"code": 400, "reason": e.to_string()}).to_string(), 
+    };
+    let mut ids = Vec::new();
 
-    json!({"code": 200, "identifiers": vec![1, 2, 3]}).to_string()
+    while let Some(result) = engine.next() {
+        match result {
+            Ok(_) => {
+                if let Some(hsh) = engine.render("../assets/rendered/.cache") { ids.push(hsh) }
+            }
+            Err(e) => return json!({"code": 400, "reason": e.to_string()}).to_string(),
+        }
+    }
+
+    json!({"code": 200, "identifiers": ids}).to_string()
+}
+
+#[delete("/api/render")]
+async fn clear_rendered() -> io::Result<()> {
+    remove_dir("../assets/rendered/.cache").await?;
+    create_dir("../assets/rendered/.cache").await?;
+
+    Ok(())
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-    .manage(EngineWrapper { engine: Mutex::new(None) })
-    .mount("/", routes![image_preview, render])
+    //.manage(EngineWrapper { engine: Mutex::new(None) })
+    .mount("/", routes![image_preview, render, clear_rendered])
 }

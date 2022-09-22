b47caca820481977161c44edca0f0b250e2986df
grostaco <grostaco@gmail.com>
2022-09-22 22:47:05 +0700

    refactor: formatting

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,10 +1,16 @@
-use log4rs::{append::{console::ConsoleAppender, file::FileAppender}, encode::pattern::PatternEncoder, Config, config::{Appender, Root, Logger}};
-use rocket::{fs::FileServer};
+use log4rs::{
+    append::{console::ConsoleAppender, file::FileAppender},
+    config::{Appender, Logger, Root},
+    encode::pattern::PatternEncoder,
+    Config,
+};
+use rocket::fs::FileServer;
 
 mod routes;
 use routes::api::{clear_rendered, image_preview, render};
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 // pub struct CORS;
 
@@ -28,7 +34,6 @@ use routes::api::{clear_rendered, image_preview, render};
 // This program should be run with cargo -p
 #[launch]
 fn rocket() -> _ {
-
     let stdout = ConsoleAppender::builder().build();
 
     let requests = FileAppender::builder()
@@ -40,16 +45,25 @@ fn rocket() -> _ {
     let config = Config::builder()
         .appender(Appender::builder().build("stdout", Box::new(stdout)))
         .appender(Appender::builder().build("requests", Box::new(requests)))
-        .logger(Logger::builder()
-            .appender("requests")
-            .additive(false)
-            .build("image_rpg", log::LevelFilter::Debug))
-        .build(Root::builder().appender("stdout").build(log::LevelFilter::Debug))
+        .logger(
+            Logger::builder()
+                .appender("requests")
+                .additive(false)
+                .build("image_rpg", log::LevelFilter::Debug),
+        )
+        .build(
+            Root::builder()
+                .appender("stdout")
+                .build(log::LevelFilter::Debug),
+        )
         .unwrap();
 
     log4rs::init_config(config).unwrap();
 
     rocket::build()
-    .mount("/", FileServer::from(concat!(env!("CARGO_MANIFEST_DIR"), "/static")))
-    .mount("/api", routes![image_preview, render, clear_rendered])
+        .mount(
+            "/",
+            FileServer::from(concat!(env!("CARGO_MANIFEST_DIR"), "/static")),
+        )
+        .mount("/api", routes![image_preview, render, clear_rendered])
 }

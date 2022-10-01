fd88434123abb8411eaef2ef6c1f21928bef1bd3
grostaco <grostaco@gmail.com>
2022-10-01 19:29:39 +0700

    feat: allow switch to play mode

--- frontend/src/components/nav.rs
+++ frontend/src/components/nav.rs
@@ -10,10 +10,11 @@ pub fn nav() -> Html {
                 <div>{{"Made with Rust!"}}</div>
             </div>
             <div class="dflex dflex-row dflex-gap-md">
-                <a href="http://127.0.0.1:8000/">{{"Home"}}</a>
-                <a href="https://github.com/grostaco">{{"GitHub"}}</a>
-                <a href="https://grostaco.herokuapp.com/">{{"About Me"}}</a>
-                <a href="https://github.com/grostaco/dVN">{{"This site's code"}}</a>
+                <a href="http://127.0.0.1:8000/">{"Home"}</a>
+                <a href="https://github.com/grostaco">{"GitHub"}</a>
+                <a href="https://grostaco.herokuapp.com/">{"About Me"}</a>
+                <a href="https://github.com/grostaco/dVN">{"This site's code"}</a>
+                <a href="http://127.0.0.1:8000/play">{"Play Mode"}</a>
             </div>
         </nav>
         <div class="divider"></div>
